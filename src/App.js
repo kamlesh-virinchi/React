e31023d5e42488c05cba953605d314b09ac6c9ea
kamleshal <kamlesh.a@virinchi.com>
2024-03-05 17:28:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Alert from "./componant/Alert";
-//import About from "./componant/About";
+import About from "./componant/About";
 import FormBhar from "./componant/FormBhar";
 import Navbar from "./componant/UparDekh";
 // import Meme from "./componant/Meme";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState("light"); //tells Dark Mode Status
@@ -24,22 +25,34 @@ function App() {
       setmode("dark");
       document.body.style.backgroundColor = "#2f3336";
       showAlert("Dark mode is enable", "success");
+      document.title = "TextUtil Dark";
     } else {
       setmode("light");
       document.body.style.backgroundColor = "#ffffff";
       showAlert("Light mode is enable", "success");
+      document.title = "TextUtil Light";
     }
   };
 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <FormBhar heading="Input de Bhai" mode={mode} showAlert={showAlert} />
-        {/* <About/> */}
-        {/* <Meme/> */}
-      </div>
+      {" "}
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <FormBhar heading="Input de Bhai" mode={mode} showAlert={showAlert} />
+            </Route>
+          </Switch>
+          <About />
+          {/* <Meme/> */}
+        </div>
+      </Router>
     </>
   );
 }
