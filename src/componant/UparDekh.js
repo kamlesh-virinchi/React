934de8a51306e0e10c24e3ca3a0a3dd55529d028
kamlesh-virinchi <kamlesh.a@virinchi.com>
2024-05-10 18:26:38 +0530

    Update UparDekh.js

--- src/componant/UparDekh.js
+++ src/componant/UparDekh.js
@@ -30,7 +30,7 @@ export default function Navbar(props) {
             <div className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"}`}>
               <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
               <label className="form-check-label " htmlFor="flexSwitchCheckDefault">
-                Enable Dark Mode Bro
+                Enable Dark Mode
               </label>
             </div>
           </div>
